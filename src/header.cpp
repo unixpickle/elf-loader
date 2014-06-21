61a448017cfcd182e712f5e53d4992e7145ade6e
Alex Nichol <unixpickle@gmail.com>
2014-06-20 20:48:06 -0400

    created ELF header struct

--- src/header.cpp
+++ src/header.cpp
@@ -7,6 +7,15 @@ Header::Header(const File & _file) : file(_file) {
 }
 
 bool Header::Init() {
+  // assert that the blob is large enough to store an ELF64 header
+  if (file.GetBlob().GetLength() < 0x40) {
+    return false;
+  }
+  
+  const uint8_t * data = (const uint8_t *)file.GetBlob().GetData();
+  if (data[4] != 2) return false; // assert 64-bit
+  info = (Header64 *)data;
+  
   return true;
 }
 
--- src/header.hpp
+++ src/header.hpp
@@ -1,9 +1,18 @@
 #ifndef __ELF_HEADER_HPP__
 #define __ELF_HEADER_HPP__
 
+#include <cstdint>
+
 namespace ELF {
 
 class File;
+
+enum ELFType {
+  ELFTypeRelocatable = 1,
+  ELFTypeExecutable,
+  ELFTypeShared,
+  ELFTypeCore,
+};
 
 class Header {
 public:
@@ -14,6 +23,38 @@ public:
   
 protected:
   const File & file;
+  
+  struct BaseHeader {
+    uint32_t magic;
+    uint8_t wordSize;
+    uint8_t endian;
+    uint8_t version1;
+    uint8_t abi;
+    uint8_t abiVersion;
+    char padding[7];
+    uint16_t type;
+    uint16_t machine;
+    uint32_t version2;
+  } __attribute__((packed));
+  
+  struct FooterInfo {
+    uint32_t flags;
+    uint16_t ehSize;
+    uint16_t phEntrySize;
+    uint16_t phEntryCount;
+    uint16_t shEntrySize;
+    uint16_t shEntryCount;
+    uint16_t sectionNamesIndex;
+  } __attribute__((packed));
+  
+  struct Header64 : public BaseHeader {
+    uint64_t entry;
+    uint64_t phOff;
+    uint64_t shOff;
+    FooterInfo footer;
+  } __attribute__((packed));
+  
+  Header64 * info; // in the future, this may be a union if I add ELF32
 };
 
 }
